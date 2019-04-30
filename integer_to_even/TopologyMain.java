9aadb71ac47099159deaae49abab491342169672
purvil <davepurvil@gmail.com>
2019-04-29 22:43:10 -0700

    Hello world storm application, emit integer and multipy it by 2.

---
+++ integer_to_even/TopologyMain.java
@@ -0,0 +1,27 @@
+package storm.purvil;
+
+import  org.apache.storm.Config;
+import  org.apache.storm.LocalCluster;
+import  org.apache.storm.topology.TopologyBuilder;
+
+public class TopologyMain {
+    public static void main(String[] args) throws InterruptedException {
+        // Build topology
+
+        TopologyBuilder builder = new TopologyBuilder();
+        builder.setSpout("My-first-spout", new MyFirstSpout());
+        builder.setBolt("my-first-bolt", new MyFirstBolt());
+
+        //Configuration
+        Config conf = new Config();
+        conf.setDebug(true);
+
+        LocalCluster cluster = new LocalCluster();
+        try {
+            cluster.submitTopology("my-first topology", conf, builder.createTopology());
+            Thread.sleep(1000);
+        } finally {
+            cluster.shutdown();
+        }
+    }
+}
